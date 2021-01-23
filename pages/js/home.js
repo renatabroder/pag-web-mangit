22e3072b633009617b858d03eb9dd268fc1738c7
Renata <renata.sendreti@gmail.com>
2021-01-23 16:01:08 -0300

    Barra de menu finalizada

--- pages/js/home.js
+++ pages/js/home.js
@@ -18,24 +18,49 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+home = (() => {
+    $('.c').removeClass('categoria-peq').addClass('categoria');
+    $('.l').removeClass('label-categoria-peq').addClass('label-categoria');
+
+    $('.container-conteudo').css('min-height', 'calc(100vh - 4rem)');
+    
+    $('.recomendados').css('display', 'none');
+    
+    $('.barra-footer').css('display', 'none');
+    $('.corpo').css('display', 'none');
+})
+
+abreMenu = (() => {
+    let menuVisivel = $('.container-menu').css('display') == 'none' ? false : true;
+
+    if(!menuVisivel){
+        $('#menu-usuario').removeClass('hide')
+        $('.container-menu').css('display', 'inline')
+    }
+    else{
+        $('#menu-usuario').addClass('hide')
+        $('.container-menu').css('display', 'none')
+    } 
+})
+
 searchButton = (() =>{
-    let campoVisivel = $('#search-field').css('display') == 'none' ? false : true;
+    let campoVisivel = $('#search-field').css('max-width') == '0px' ? false : true;
     let valCampo = $('#search-field').val()
 
-    if(!campoVisivel) $('#search-field').css('display', 'inline')
-    else if(campoVisivel && ( valCampo == '')) $('#search-field').css('display', 'none')
+    if(!campoVisivel) $('#search-field').removeClass('hide')
+    else if(campoVisivel && ( valCampo == '')) $('#search-field').addClass('hide')
     else if(valCampo != '') pesquisa();        
 });
 
 focusoutSearchButton = (() =>{
-    if($('#search-field').val() == "") $('#search-field').css('display', 'none')
+    if($('#search-field').val() == "") $('#search-field').addClass('hide')
 })
 
 cliqueCategoria = ((tipo) => {
     $('.c').removeClass('categoria').addClass('categoria-peq');
     $('.l').removeClass('label-categoria').addClass('label-categoria-peq');
 
-    $('.barra').css('display', 'flex');
+    $('.barra-footer').css('display', 'flex');
     $('.container-conteudo').css('min-height', 'calc(100vh - 9rem)');
 
     $('.recomendados').css('display', 'block');
